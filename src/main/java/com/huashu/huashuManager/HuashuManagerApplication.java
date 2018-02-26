8b6220971037568de616d4f46e3e14a551c2faed
zhanghao@utry.cn <zhanghao@utry.cn>
2018-02-26 20:08:49 +0800

    初始化项目

---
+++ src/main/java/com/huashu/huashuManager/HuashuManagerApplication.java
@@ -0,0 +1,12 @@
+package com.huashu.huashuManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuashuManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuashuManagerApplication.class, args);
+	}
+}
